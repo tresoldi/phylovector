80d1fb38bea9db7396ae1f038e3eaffa0840b9c6
Tiago Tresoldi <tresoldi@gmail.com>
2021-10-11 11:52:04 +0200

    Add scaffolding for tests

---
+++ tests/test_phylovector.py
@@ -0,0 +1,12 @@
+"""
+test_phylovector
+================
+
+Tests for the `phylovector` package.
+"""
+
+# Import the library being tested
+import phylovector
+
+def test_dummy():
+    assert phylovector.dummy() == 13
