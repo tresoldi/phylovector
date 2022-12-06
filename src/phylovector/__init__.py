d06908edc60c7ddd5e99d233290e54a1c7570d17
Tiago Tresoldi <tresoldi@shh.mpg.de>
2022-12-06 15:13:23 +0100

    add vector_length function

--- src/phylovector/__init__.py
+++ src/phylovector/__init__.py
@@ -12,6 +12,9 @@ from .phylovector import tree2vector, vector2tree
 from .common import sorted_newick, start_vector
 from .likelihood import method1, primate
 
+def vector_length(num_taxa):
+    return int((pow(num_taxa, 2) + num_taxa) / 2.0)
+
 # Build the namespace
 __all__ = [
     "sorted_newick",
@@ -20,4 +23,5 @@ __all__ = [
     "method1",
     "start_vector",
     "primate",
+    "vector_length",
 ]
